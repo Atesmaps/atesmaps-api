5767f94f9b35fa2653f4028f03c7ce83772fe93c
Roger Calaf <rcalaf.cot@gmail.com>
2022-11-26 15:44:09 +1100

    observations
    # Conflicts:
    #	controllers/authController.js
    #	controllers/observationsController.js
    #	controllers/usersController.js
    #	model/Observation.js
    #	routes/api/users.js
    #	server.js

--- controllers/observationsController.js
+++ controllers/observationsController.js
@@ -1,125 +1,36 @@
 const Observation = require('../model/Observation');
 const User = require('../model/User');
-const GeoJSON = require('geojson');
-const proj4 = require('proj4');
-const { format } = require('date-fns');
-//const { sendNotification } = require('../services/notificationService');
 
 
 const getAllObservations = async (req, res) => {
-    let observations = null;
-    console.log(req.query);
-    if(req.query.days && req.query.long && req.query.lat ){
-        //Both days & location filter.
-        var endDate = new Date();
-        var startDate = new Date();
-        var day = endDate.getDate() - Number(req.query.days);
-        startDate.setDate(day);
-        // observations = await Observation.find({
-        //     $and:[
-        //         {date: { $gte: startDate }},
-        //         {location: {$near: 
-        //             {$geometry: {
-        //                 type: 'Point',
-        //                 coordinates: [req.query.long, req.query.lat]
-        //             },
-        //             $maxDistance: 80000,
-        //         }}}
-        //     ]}).sort({date: 'desc'}).populate('user');
-        observations = await Observation.find({
-                    date: { $gte: startDate }  
-                }).sort({date: 'desc'}).populate('user');
-    }else if(req.query.days && !(req.query.long && req.query.lat)){
-        //Only days filter.
-        var endDate = new Date();
-        var startDate = new Date();
-        var day = endDate.getDate() - Number(req.query.days);
-        startDate.setDate(day);
-        observations = await Observation.find({
-            $and:[
-                {date: { $gte: startDate }}
-            ]}).sort({date: 'desc'}).populate('user');
-    }else if(!req.query.days && (req.query.long && req.query.lat)){
-        //Only Location filter.
-        // observations = await Observation.find({
-        //     $and:[
-        //         {location: {$near: 
-        //             {$geometry: {
-        //                 type: 'Point',
-        //                 coordinates: [req.query.long, req.query.lat]
-        //             },
-        //             $maxDistance: 80000,
-        //         }}}
-        //     ]}).sort({date: 'desc'}).populate('user');
-        observations = await Observation.find({}).sort({date: 'desc'}).populate('user');
-    }else{
-        //No filter.
-        observations = await Observation.find().sort({date: 'desc'}).populate('user');
-    }
-    // console.log(observations.length)
-    // if (!observations) return res.status(204).json({ 'message': 'No observations found' });
+    const observations = await Observation.find();
+    //if (!observations) return res.status(204).json({ 'message': 'No observations found' });
     res.json(observations);
 }
 
 const getUserObservations = async (req, res) => {
     if (!req?.params?.id) return res.status(400).json({ "message": 'User ID required' });
-    console.log(req?.query?.page);
-    //TODO: add pagination filter +  Date filter
-    const limit = 5;
-    let options = {}
-    if(req?.query?.page){
-        //let page = (!req?.query?.page ? req?.query?.page : 1);
-        options = { limit: limit, skip: limit*(req.query.page-1), sort: [{"date": "desc" }] };
-    }else{
-        options = { sort: [{"date": "desc" }] };
-    }
-    console.log(options)
-    const user = await User.findOne({ _id: req.params.id }).populate({path:'observations',options}).exec();
+    const user = await User.findOne({ _id: req.params.id }).populate('observations').exec();
     if (!user) return res.status(204).json({ 'message': 'No users found' });
     const observations = user.observations;
-//  console.log(observations);
+    console.log(user);
     res.json(observations);
 }
 
 const createNewObservation = async (req, res) => {
-    // console.log('This is a new oservation body received...')
-    // console.log(req.body.observationTypes.quick);
+    // if (!req?.body?.firstname || !req?.body?.lastname) {
+    //     return res.status(400).json({ 'message': 'First and last names are required' });
+    // }
     const user = await User.findOne({ _id: req.body.user });
     if (!user) return res.status(204).json({ 'message': 'No users found' });
     req.body.user = user;
-    req.body.status = 1; 
-    req.body.location = { type: 'Point', coordinates: [  req.body.location.longitude, req.body.location.latitude] };
+
     try {
         const result = await Observation.create(req.body);
-        // console.log(req.body.observationTypes.snowpack);
         user.observations.push(result.toObject({ getters: true }));
-        let saveResult = await user.save();
-
-        // Notification logic:
-        // const usersToNotify = await User.find(
-        //     { _id: { $ne: user.id } }, // $ne = Not Equal
-        //     'deviceTokens' // Only select the 'deviceTokens' field for efficiency
-        // );
-
-        // Flatten the array of tokens from all users
-        const allTargetTokens = usersToNotify
-            .flatMap(user => user.deviceTokens) // Get all token objects
-            .map(tokenDoc => tokenDoc.token); // Get just the token string
-
-        // Send the notification
-        if (allTargetTokens.length > 0) {
-            // sendNotification(
-            //     allTargetTokens,
-            //     "New Observation Posted", // Title
-            //     `${newObservation.user.username} just posted a new observation.`, // Body
-            //     { observationId: newObservation._id.toString() } // Custom data
-            // );
-        }
-
-        // console.log(saveResult)
-        return res.status(201).json({'observations': user.observations, 'observationId': result._id});
+        await user.save();
+        return res.status(201).json(result);
     } catch (err) {
-        console.log('error')
         console.error(err);
         res.status(500).json({ 'message': 'Error creating observation' });
     }
@@ -134,16 +45,17 @@ const updateObservation = async (req, res) => {
     if (!observation) {
         return res.status(204).json({ "message": `No observation matches ID ${req.body.id}.` });
     }
-
+    if (req.body?.firstname) employee.firstname = req.body.firstname;
+    if (req.body?.lastname) employee.lastname = req.body.lastname;
     const result = await observation.save();
     res.json(result);
 }
 
 
 const deleteObservation = async (req, res) => {
-    // console.log('------ Observation Controller delete action -----');
-    // console.log(req.body._id);
-    // console.log('------ END Observation Controller delete action -----');
+    console.log('------ Observation Controller delete action -----');
+    console.log(req.body._id);
+    console.log('------ END Observation Controller delete action -----');
     if (!req.body?._id) return res.status(400).json({ "message": 'Observation ID required' });
     const observation = await Observation.findOne({ _id: req.body._id }).populate('user').exec();
     if (!observation) {
@@ -162,80 +74,6 @@ const getObservation = async (req, res) => {
     res.json(observation);
 }
 
-const getFeatures = async (req, res) => {
-    const box = [[parseFloat(req.query.transformedbbox[0]),parseFloat(req.query.transformedbbox[1])],[parseFloat(req.query.transformedbbox[2]),parseFloat(req.query.transformedbbox[3])]];
-    // const box = [[parseFloat(req.query.transformedbbox[3]),parseFloat(req.query.transformedbbox[2])],[parseFloat(req.query.transformedbbox[1]),parseFloat(req.query.transformedbbox[0])]];
-    let data = await Observation.find({$and:[{date: { $gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)}, location: {
-        $geoWithin: {
-            $box: box,
-        }
-    }}]}).sort({date: 'desc'}).populate('user');
-    // console.log('--------')
-    // console.log(data);
-    // console.log('--------')
-    let features = [];
-    data.forEach((feature)=>{
-        let terrainScore = 0;
-        let userTa =  -1;
-        let userRa =  -1;
-        if(feature.user){
-            switch(feature.user.terrainType) {
-                case 1:
-                    terrainScore = 0.5
-                break;
-                case 2:
-                    terrainScore = 1
-                break;
-                case 3:
-                    terrainScore = 3
-                break;
-                default:
-                    terrainScore = 0
-            }
-            //let transformedCoors = proj4(proj4.defs('EPSG:4326'),proj4.defs('EPSG:3857'),[feature.location.coordinates[1],feature.location.coordinates[0]]);
-            userTa = (feature.user.professionalOrientation-1)+(feature.user.snowEducationLevel-1)+(feature.user.snowExperienceLevel*terrainScore);
-            userRa = feature.user.avalanchExposure + feature.user.conditionsType;
-        }else{
-            terrainScore = -1;
-        }
-      
-        // console.log(feature.user);
-        features.push({
-            "type": "Feature",
-            "geometry": feature.location,
-            "properties":{
-                "type": "observation",
-                "title":feature.title,
-                "date":feature.date,
-                "directoryId":feature.directoryId,
-                "images":feature.images,
-                "observationTypes":feature.observationTypes,
-                "user": {
-                    "username": feature.user ? feature.user.username : "annonymous",
-                    "name": feature.user ? feature.user.name : "annonymous",
-                    "lastName": feature.user ? feature.user.lastName : "annonymous",
-                    "Ta": userTa,
-                    "Ra": userRa,
-                }
-            }
-        });
-    })
-    res.json({
-        "type": "FeatureCollection",
-        "crs": {
-            "type": "name",
-            "properties": {
-              "name": "EPSG:4326",
-            },
-        },
-        "numberMatched":features.length,
-        "numberReturned":features.length,
-        "timeStamp": new Date(),
-        "totalFeatures":features.length,
-        "features":features
-    });
-}
-
 module.exports = {
     getAllObservations,
     getUserObservations,
@@ -243,5 +81,4 @@ module.exports = {
     updateObservation,
     deleteObservation,
     getObservation,
-    getFeatures
 }
