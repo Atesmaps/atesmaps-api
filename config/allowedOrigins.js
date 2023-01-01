7a0925b9428340203b799e4d21ec5f868ae96280
Roger Calaf <rcalaf.cot@gmail.com>
2023-01-01 21:00:08 +1100

    return observations features via bbox strategy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,11 @@
 const allowedOrigins = [
     'https://atesmaps.org',
-    'https://test.atesmaps.org',
-    'https://experimental.atesmaps.org',
-    'http://localhost:5000',
-    'https://obs.atesmaps.org',
-    'https://map.lauegi.maladeta.studio/'
+    'https://api.atesmaps.org',
+    'https://api-test.atesmaps.org/',
+    'http://127.0.0.1:3500',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:5000'
 ];
 
 module.exports = allowedOrigins;
