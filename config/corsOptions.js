2801efeed5275723fe84fc6c88d52aee3986b520
Roger Calaf <rcalaf.cot@gmail.com>
2023-01-09 00:06:03 +1100

    v4.1

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,26 +1,16 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-  
     origin: (origin, callback) => {
-        console.log(origin)
+        console.log(origin);
+        console.log(allowedOrigins.indexOf(origin));
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
+            callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
-    preflightContinue: false,
-    credentials: true,
-    maxAge:600,
-    optionsSuccessStatus: 204
+    optionsSuccessStatus: 200
 }
-
-// const corsOptions = {
-//     origin: true,
-//     optionsSuccessStatus: 200
-// }
-
 
 module.exports = corsOptions;
