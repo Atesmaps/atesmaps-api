1b425229d338bd24c9b68b3c39fb812f7efa8f46
Roger Calaf <rcalaf@Mac.fritz.box>
2025-11-20 21:37:00 +0100

    added notifications logic -- but disabled --

--- model/User.js
+++ model/User.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const crypto = require('crypto');
 
+const DeviceTokenSchema = new Schema({
+    token: {
+        type: String,
+        required: true,
+    },
+    os: {
+        type: String,
+        required: true,
+        enum: ['ios', 'android']
+    }
+}, {_id: false});
+
 const userSchema = new Schema({
     status: {
         type: Boolean,
@@ -133,6 +145,7 @@ const userSchema = new Schema({
     //     ref: "Subscription",
     //     required: false
     // }]
+    deviceTokens: [DeviceTokenSchema]
 },
 {
   timestamps: true
