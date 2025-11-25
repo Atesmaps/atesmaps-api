37c038c9eb1a6976cfbf20ca28d09e2c363478ce
Roger Calaf <rcalaf@Mac.fritz.box>
2025-11-25 12:01:53 +0100

    fixed cors and headers handling

--- controllers/lauegiController.js
+++ controllers/lauegiController.js
@@ -21,7 +21,24 @@ const getObservation = async (req, res) => {
     res.json(observation);
 }
 
+const getFeatures2 = async (req, res) => {
+    console.log('--- DUMMY TEST START ---');
+    const providedApiKey = req.headers['x-api-key']; 
+    
+    // Check if middleware already ruined the response
+    if (res.headersSent) {
+        console.log('CRITICAL: Headers already sent by middleware!');
+        return;
+    }
+
+    console.log('Key received:', providedApiKey);
+    
+    // Send a simple, immediate success
+    return res.status(200).json({ message: "Routing is working! The bug is in the DB logic." });
+}
+
 const getFeatures = async (req, res) => {
+
     const apiKey = process.env.LAUEGI_API_KEY; 
     const providedApiKey = req.headers['x-api-key']; 
 
@@ -29,86 +46,93 @@ const getFeatures = async (req, res) => {
         return res.status(401).json({ "message": 'Unauthorized: Invalid or missing API key.' });
     }
 
+    try {
 
-    //const box = [[parseFloat(req.query.transformedbbox[0]),parseFloat(req.query.transformedbbox[1])],[parseFloat(req.query.transformedbbox[2]),parseFloat(req.query.transformedbbox[3])]];
-    const box = [[parseFloat(0.6402078369020309),parseFloat(42.582312510993745)],[parseFloat(1.0246806029244835),parseFloat(42.868976713679956)]];
-    //  console.log(`$and:[{date: { $gte: ${new Date(req.query.startDate)}, $lte: ${new Date(req.query.endDate)}}, location: {
-    //     $geoWithin: {
-    //         $box: ${box},
-    //     }
-    // }}]}`)
-    let data = await Observation.find({$and:[{date: { $gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)}, location: {
-        $geoWithin: {
-            $box: box,
-        }
-    }}]}).sort({date: 'desc'}).populate('user');
-
-   
-    // console.log('--------')
-    // console.log(data);
-    // console.log('--------')
-    let features = [];
-    data.forEach((feature)=>{
-        let terrainScore = 0;
-        let userTa =  -1;
-        let userRa =  -1;
-        if(feature.user){
-            switch(feature.user.terrainType) {
-                case 1:
-                    terrainScore = 0.5
-                break;
-                case 2:
-                    terrainScore = 1
-                break;
-                case 3:
-                    terrainScore = 3
-                break;
-                default:
-                    terrainScore = 0
+        //const box = [[parseFloat(req.query.transformedbbox[0]),parseFloat(req.query.transformedbbox[1])],[parseFloat(req.query.transformedbbox[2]),parseFloat(req.query.transformedbbox[3])]];
+        const box = [[parseFloat(0.6402078369020309),parseFloat(42.582312510993745)],[parseFloat(1.0246806029244835),parseFloat(42.868976713679956)]];
+        //  console.log(`$and:[{date: { $gte: ${new Date(req.query.startDate)}, $lte: ${new Date(req.query.endDate)}}, location: {
+        //     $geoWithin: {
+        //         $box: ${box},
+        //     }
+        // }}]}`)
+        let data = await Observation.find({$and:[{date: { $gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)}, location: {
+            $geoWithin: {
+                $box: box,
             }
-            //let transformedCoors = proj4(proj4.defs('EPSG:4326'),proj4.defs('EPSG:3857'),[feature.location.coordinates[1],feature.location.coordinates[0]]);
-            userTa = (feature.user.professionalOrientation-1)+(feature.user.snowEducationLevel-1)+(feature.user.snowExperienceLevel*terrainScore);
-            userRa = feature.user.avalanchExposure + feature.user.conditionsType;
-        }else{
-            terrainScore = -1;
-        }
-      
-        // console.log(feature.user);
-        features.push({
-            "type": "Feature",
-            "id": feature._id,
-            "geometry": feature.location,
-            "properties":{
-                "type": "observation",
-                "title":feature.title,
-                "date":feature.date,
-                "directoryId":feature.directoryId,
-                "images":feature.images,
-                "observationTypes":feature.observationTypes,
-                "user": {
-                    "username": feature.user ? feature.user.username : "annonymous",
-                    "name": feature.user ? feature.user.name : "annonymous",
-                    "lastName": feature.user ? feature.user.lastName : "annonymous",
-                    "Ta": userTa,
-                    "Ra": userRa,
+        }}]}).sort({date: 'desc'}).populate('user');
+
+    
+        // console.log('--------')
+        // console.log(data);
+        // console.log('--------')
+        let features = [];
+        data.forEach((feature)=>{
+            let terrainScore = 0;
+            let userTa =  -1;
+            let userRa =  -1;
+            if(feature.user){
+                switch(feature.user.terrainType) {
+                    case 1:
+                        terrainScore = 0.5
+                    break;
+                    case 2:
+                        terrainScore = 1
+                    break;
+                    case 3:
+                        terrainScore = 3
+                    break;
+                    default:
+                        terrainScore = 0
                 }
+                //let transformedCoors = proj4(proj4.defs('EPSG:4326'),proj4.defs('EPSG:3857'),[feature.location.coordinates[1],feature.location.coordinates[0]]);
+                userTa = (feature.user.professionalOrientation-1)+(feature.user.snowEducationLevel-1)+(feature.user.snowExperienceLevel*terrainScore);
+                userRa = feature.user.avalanchExposure + feature.user.conditionsType;
+            }else{
+                terrainScore = -1;
             }
-        });
-    })
-    res.json({
-        "type": "FeatureCollection",
-        "crs": {
-            "type": "name",
-            "properties": {
-              "name": "EPSG:4326",
+        
+            // console.log(feature.user);
+            features.push({
+                "type": "Feature",
+                "id": feature._id,
+                "geometry": feature.location,
+                "properties":{
+                    "type": "observation",
+                    "title":feature.title,
+                    "date":feature.date,
+                    "directoryId":feature.directoryId,
+                    "images":feature.images,
+                    "observationTypes":feature.observationTypes,
+                    "user": {
+                        "username": feature.user ? feature.user.username : "annonymous",
+                        "name": feature.user ? feature.user.name : "annonymous",
+                        "lastName": feature.user ? feature.user.lastName : "annonymous",
+                        "Ta": userTa,
+                        "Ra": userRa,
+                    }
+                }
+            });
+        })
+        res.json({
+            "type": "FeatureCollection",
+            "crs": {
+                "type": "name",
+                "properties": {
+                "name": "EPSG:4326",
+                },
             },
-        },
-        "numberMatched":features.length,
-        "numberReturned":features.length,
-        "timeStamp": new Date(),
-        "totalFeatures":features.length,
-        "features":features
-    });
+            "numberMatched":features.length,
+            "numberReturned":features.length,
+            "timeStamp": new Date(),
+            "totalFeatures":features.length,
+            "features":features
+        });
+    } catch (err) {
+        // 7. Error Handling
+        console.error("Error in getFeatures:", err);
+        // This prevents the request from hanging if DB fails
+        return res.status(500).json({ "message": "Server Error", "error": err.message });
+    }
 }
 
 module.exports = {
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,27 @@
 const { logEvents } = require('./logEvents');
 
+// const errorHandler = (err, req, res, next) => {
+//     console.log('SSSSSHIIITTTT')
+//     logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
+//     console.error(err.stack)
+//     res.status(500).send(err.message);
+// }
 const errorHandler = (err, req, res, next) => {
+    // 1. Log the error internally
     logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
-    console.error(err.stack)
+    console.error(err.stack);
+
+    // 2. SAFETY CHECK: 
+    // If the response was already sent (by the controller), 
+    // we CANNOT send another one. We just delegate to the default Express handler.
+    if (res.headersSent) {
+        console.log('Error handler called, but headers were already sent. Delegating to default handler.');
+        return next(err);
+    }
+
+    // 3. If headers weren't sent, it's safe to send the 500 error
     res.status(500).send(err.message);
 }
 
 module.exports = errorHandler;
+
