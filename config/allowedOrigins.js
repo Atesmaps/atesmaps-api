24a07782ceccc30a86da87d42efe7148561fd879
Roger Calaf <rcalaf@Mac.fritz.box>
2025-11-25 15:43:27 +0100

    CORS test

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
-    'https://atesmaps.org',
-    'https://test.atesmaps.org',
-    'https://experimental.atesmaps.org',
+    // 'https://atesmaps.org',
+    // 'https://test.atesmaps.org',
+    // 'https://experimental.atesmaps.org',
     'http://localhost:5000',
-    'https://obs.atesmaps.org',
+    // 'https://obs.atesmaps.org',
     'https://map.lauegi.maladeta.studio'
 ];
 
