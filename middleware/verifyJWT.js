00898f36c11804cc5d01b87ace7fa8b3ba2184bd
Roger Calaf <rcalaf@Mac.fritz.box>
2025-11-25 12:01:53 +0100

    fixed cors and headers handling

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,6 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
+    if (res.headersSent) {
+        return next(); 
+    }
+
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
