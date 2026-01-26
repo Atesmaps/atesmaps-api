098b61c91a5acfe7769eda20e4ea88aee1e77d9b
Roger Calaf <rcalaf@Rogers-MacBook-Pro.local>
2026-01-26 11:38:09 +0100

    update versions

--- controllers/authController.js
+++ controllers/authController.js
@@ -285,7 +285,7 @@ const handleAppleLogin = async (req, res) => {
 }
 
 const showVersion = (req, res) => {
-    res.status(200).json({ 'version': '1.0.4' });
+    res.status(200).json({ 'version': '1.0.5' });
 }
 
 const showIosVersion = (req, res) => {
